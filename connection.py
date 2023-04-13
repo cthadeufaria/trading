f90d0751479d5a45d03c65848966c1df13877606
cthadeufaria <crls.thadeu@gmail.com>
2023-04-13 10:55:11 +0100

    Connection class

---
+++ connection.py
@@ -0,0 +1,64 @@
+"""Any connection with available exchanges."""
+import os, requests, hmac, hashlib
+from urllib.parse import urlencode
+
+class Connection:
+    def __init__(self, environment='production') -> None:
+        self.environment = environment
+        self.endpoints = {}
+
+        # /api/ endpoints
+        endpoints = {
+            'test' : '/api/v3/ping',
+            'server_time' : '/api/v3/time',
+            'exchange_info' : '/api/v3/exchangeInfo',
+            'order_book' : '/api/v3/depth',
+            'candlestick' : '/api/v3/klines',
+            'avg_price' : '/api/v3/avgPrice',
+            'best_price' : '/api/v3/ticker/bookTicker',
+            'acc_info' : '/api/v3/account',
+            'acc_snapshot' : '/sapi/v1/accountSnapshot',
+            'price' : '/api/v3/ticker/price',
+            'price_hist' : '/api/v3/historicalTrades',
+            'order' : '/api/v3/order',
+            'test_order' : '/api/v3/order/test',
+            'trades' : '/api/v3/myTrades',
+            'options_info' : '/vapi/v1/optionInfo',
+            'options_order_book' : '/vapi/v1/depth',
+            'options_mark_price' : '/vapi/v1/mark',
+        }
+
+        if self.environment == 'test':
+            main_endpoint = 'https://testnet.binance.vision'
+            self.auth_dict = {
+                'key' : os.environ.get('TEST_KEY'),
+                'skey' : os.environ.get('TEST_SKEY'),
+            }
+        elif self.environment == 'production':
+            main_endpoint = 'https://api1.binance.com'
+            options_endpoint = 'https://vapi.binance.com'
+            self.auth_dict = {
+                'key' : os.environ.get('SPOT_KEY'),
+                'skey' : os.environ.get('SPOT_SKEY'),
+            }
+
+        # complete endpoints strings
+        for endpoint in endpoints:
+            if endpoint[0:7] == 'options':
+                self.endpoints[endpoint] = options_endpoint + endpoints[endpoint]
+            else:
+                self.endpoints[endpoint] = main_endpoint + endpoints[endpoint]
+            
+        print(self.endpoints)
+
+    def ping(self) -> None:
+        r = requests.get(self.endpoints['test'])
+        print('ping: ' + str(r))
+
+    def sha256_signature(self, endpoint_params) -> None:
+        secret = self.auth_dict['skey']
+        params = urlencode(endpoint_params)
+        hashedsig = hmac.new(secret.encode('utf-8'), params.encode('utf-8'), hashlib.sha256).hexdigest()
+        self.hashedsig_dict = {
+            "signature" : hashedsig
+        }
